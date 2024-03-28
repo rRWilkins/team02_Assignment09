b3cb093da3d3cc34ec51de58f29942cf6f4a3566
rRWilkins <157031289+rRWilkins@users.noreply.github.com>
2024-03-28 09:01:29 -0400

    Updated main.py

--- Team2_Assignment09/mainPackage/main.py
+++ Team2_Assignment09/mainPackage/main.py
@@ -1,5 +1,21 @@
-'''
-Created on Mar 28, 2024
+import json
+import requests
 
-@author: kleinbw
-'''
+if __name__ == "__main__":
+    
+    song = input("Give me an album to search for:")
+
+    url = "https://spotify23.p.rapidapi.com/search"
+
+    querystring = {"type": "album", "q": song}  # Replace "Born in the U.S.A." with the name of the album you want to search for
+
+    headers = {
+        "X-RapidAPI-Key": "e354a39aecmsh2c967112f19af0dp10dd88jsn9435c59da7e1",
+        "X-RapidAPI-Host": "spotify23.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    print(response.json())
+
+    
